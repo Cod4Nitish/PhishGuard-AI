5765c6bf04384a9bee138e6508b5b1bfd4a30b30
Nitish Kumar Singh <cod4nitish@users.noreply.github.com>
2026-04-07 16:02:39 +0530

    Fix: HashRouter proper setup for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import LiveGuard from './pages/LiveGuard';
@@ -8,11 +8,10 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="monitoring" element={<LiveGuard />} />
-          <Route path="history" element={<History />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
+        <Route element={<MainLayout />}>
+          <Route index path="/" element={<Home />} />
+          <Route path="/monitoring" element={<LiveGuard />} />
+          <Route path="/history" element={<History />} />
         </Route>
       </Routes>
     </HashRouter>
